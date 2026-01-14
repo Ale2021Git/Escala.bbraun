8562cc9683081985bb160e2803a909e7652c7e94
Ale2021Git <86354193+Ale2021Git@users.noreply.github.com>
2026-01-14 13:15:48 -0300

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,15 @@
+{
+  "name": "B.Braun Escala & Planejamento",
+  "short_name": "Escala B.Braun",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#00A97A",
+  "theme_color": "#00A97A",
+  "icons": [
+    {
+      "src": "https://img.icons8.com/color/512/calendar.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
